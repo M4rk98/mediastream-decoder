a64d52ee21aeaf83276148003846b3ad7b3b56ed
MarkShepper <mark.gangel@shepper.com>
2023-12-08 00:11:47 +0000

    update home page with title, description

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 
+import VideoCapture from "components/video-capture";
+
+const content = {
+  title: "Frame Forges",
+  description:
+    "Click &quot;Start&quot; to record an amazing 10s long video and check out 20 frames from it",
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {content.title}
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            {content.description}
+          </p>
+        </div>
+        <VideoCapture />
+      </div>
     </div>
   );
 }
